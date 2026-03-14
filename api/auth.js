151bfa9d91acfe5250cd8436700bd1c92e45aa81
hotier <1976277790@qq.com>
2026-03-14 21:21:30 +0800

    fix

--- api/auth.js
+++ api/auth.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
   const clientId = process.env.GITHUB_CLIENT_ID;
   
   // 统一使用 Vercel 项目域名作为回调地址
-  const redirectUri = `https://site-navy-beta.vercel.app/api/callback?provider=${provider}&site_id=${site_id}&scope=${scope}`;
+  const redirectUri = `https://hotier.cc.cd/api/callback?provider=${provider}&site_id=${site_id}&scope=${scope}`;
   
   const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scope || 'repo,user'}&redirect_uri=${encodeURIComponent(redirectUri)}`;
   
